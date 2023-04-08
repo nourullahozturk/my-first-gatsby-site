7219650653c16a350bae8e35e08476a4d5b2f518
nourullahozturk <nourullahozturk@gmail.com>
2023-04-08 08:31:08 +0300

    Finished Gatsby Tutorial Part 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}\\blog`,
+        path: `${__dirname}/blog`,
       },
     },
     `gatsby-plugin-mdx`,
